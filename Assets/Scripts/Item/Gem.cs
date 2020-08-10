6f61ae1bb5fde930ce62bbfc851ed3da582a6a7f
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2020-08-10 20:25:13 -0300

    Adds Script for Gem's behaviour

---
+++ Assets/Scripts/Item/Gem.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Gem script.
+/// </summary>
+public class Gem : MonoBehaviour
+{
+
+    public bool inverted;
+
+    private AnimationCurve curve;
+    private Vector3 gemPosition;
+
+    void Start()
+    {
+        curve = new AnimationCurve(new Keyframe(0, 0), new Keyframe(0.8f, 0.2f));
+        curve.preWrapMode = WrapMode.PingPong;
+        curve.postWrapMode = WrapMode.PingPong;
+
+        gemPosition = transform.position;
+    }
+
+    void Update()
+    {
+        if (inverted)
+        {
+            transform.position = new Vector3(gemPosition.x, gemPosition.y - curve.Evaluate(Time.time), gemPosition.z);
+        }
+        else
+        {
+            transform.position = new Vector3(gemPosition.x, gemPosition.y + curve.Evaluate(Time.time), gemPosition.z);
+        }
+
+
+    }
+
+}
