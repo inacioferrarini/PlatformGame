e57c4c169a4047fb637897287ccdaa53e0848407
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2020-08-16 17:37:43 -0300

    Renames LevelData to LevelObject.

---
+++ Assets/Script/LevelObjects.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class LevelObjects : MonoBehaviour
+{
+    public AudioSource m_playerAudioPlayer;
+    public AudioSource m_itemAudioPlayer;
+
+    private void Start()
+    {
+        SoundManager.instance.SetPlayerAudioPlayer(m_playerAudioPlayer);
+        SoundManager.instance.SetItemAudioPlayer(m_itemAudioPlayer);
+    }
+
+}
