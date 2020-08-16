83b51034f3e18bc8c196778a6323ee44863b829a
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2020-08-16 16:53:47 -0300

    Improves script

--- Assets/Script/Manager/GameManager.CollisionManager.cs
+++ Assets/Script/Manager/GameManager.CollisionManager.cs
@@ -5,13 +5,20 @@
 /// </summary>
 public partial class GameManager
 {
-
     /// <summary>
     /// Manages collisions between two different game objects.
     /// </summary>
     class CollisionManager
     {
         private GameManager mp_gameManager;
+
+        /// <summary>
+        /// Possible game status.
+        /// </summary>
+        public enum CollisionMembers
+        {
+            PLAYER_GEM, PLAYER_EXIT, PLAYER_ENEMY, NONE
+        }
 
         public CollisionManager(GameManager p_gameManager)
         {
@@ -20,41 +27,62 @@ public partial class GameManager
 
         public void HandleCollision(GameObject object1, GameObject object2)
         {
+            CollisionMembers collisionMembers = EvaluateCollision(object1, object2);
+            if (collisionMembers == CollisionMembers.PLAYER_GEM)
+            {
+                CollectableItem item = SafeGetCollectableItem(object1, object2);
+                HandlePlayerGetGem(item);
+            }
+            else if (collisionMembers == CollisionMembers.PLAYER_EXIT)
+            {
+                Player player = SafeGetPlayer(object1, object2);
+                HandlePlayerExit(player);
+            }
+            else if (collisionMembers == CollisionMembers.PLAYER_ENEMY)
+            {
+                Player player = SafeGetPlayer(object1, object2);
+                HandlePlayerDie(player);
+            }
+        }
+
+        private CollisionMembers EvaluateCollision(GameObject object1, GameObject object2)
+        {
             if ((object1.CompareTag(Constants.Collision.Tags.player) && object2.CompareTag(Constants.Collision.Tags.gem))
                 || (object1.CompareTag(Constants.Collision.Tags.gem) && object2.CompareTag(Constants.Collision.Tags.player)))
             {
-                CollectableItem item = object1.GetComponent<CollectableItem>();
-                if (object2.CompareTag(Constants.Collision.Tags.gem))  // TODO: Change this tag to Item
-                {
-                    item = object2.GetComponent<CollectableItem>();
-                }
-
-                HandlePlayerGetGem(item);
+                return CollisionMembers.PLAYER_GEM;
             }
-
-            if ((object1.CompareTag(Constants.Collision.Tags.player) && object2.CompareTag(Constants.Collision.Tags.exit))   // TODO: Improve this . Create a method that returns an Enum.
+            else if ((object1.CompareTag(Constants.Collision.Tags.player) && object2.CompareTag(Constants.Collision.Tags.exit))
                 || (object1.CompareTag(Constants.Collision.Tags.exit) && object2.CompareTag(Constants.Collision.Tags.player)))
             {
-                Player player = object1.GetComponent<Player>();
-                if (object2.CompareTag(Constants.Collision.Tags.player))
-                {
-                    player = object2.GetComponent<Player>();
-                }
-
-                HandlePlayerExit(player);
+                return CollisionMembers.PLAYER_EXIT;
             }
-
-            if ((object1.CompareTag(Constants.Collision.Tags.player) && object2.CompareTag(Constants.Collision.Tags.enemy))   // TODO: Improve this . Create a method that returns an Enum.
+            else if ((object1.CompareTag(Constants.Collision.Tags.player) && object2.CompareTag(Constants.Collision.Tags.enemy))
                 || (object1.CompareTag(Constants.Collision.Tags.enemy) && object2.CompareTag(Constants.Collision.Tags.player)))
             {
-                Player player = object1.GetComponent<Player>();
-                if (object2.CompareTag(Constants.Collision.Tags.player))
-                {
-                    player = object2.GetComponent<Player>();
-                }
-
-                HandlePlayerDie(player);
+                return CollisionMembers.PLAYER_ENEMY;
             }
+            return CollisionMembers.NONE;
+        }
+
+        private CollectableItem SafeGetCollectableItem(GameObject object1, GameObject object2)
+        {
+            CollectableItem item = object1.GetComponent<CollectableItem>();
+            if (object2.CompareTag(Constants.Collision.Tags.gem))
+            {
+                item = object2.GetComponent<CollectableItem>();
+            }
+            return item;
+        }
+
+        private Player SafeGetPlayer(GameObject object1, GameObject object2)
+        {
+            Player player = object1.GetComponent<Player>();
+            if (object2.CompareTag(Constants.Collision.Tags.player))
+            {
+                player = object2.GetComponent<Player>();
+            }
+            return player;
         }
 
         private void HandlePlayerGetGem(CollectableItem item)
