6d206c28c6095d0b81788910bb14dbbd5b991592
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2020-09-01 21:12:40 -0300

    Adds Joystick

--- Assets/Scripts/Characters/Player/Player.cs
+++ Assets/Scripts/Characters/Player/Player.cs
@@ -7,6 +7,7 @@
 public class Player : Character
 {
     public int jumpForce;
+    public Joystick joystick;
 
     // Player State
     private bool isJumping = false;
@@ -34,7 +35,8 @@ public class Player : Character
         animator.SetBool(AnimationVariables.isLevelComplete, levelCompleted);
         animator.SetBool(AnimationVariables.isRunning, isRunning);
 
-        if (Input.GetButtonDown(Constants.Input.Keys.jump) && grounded)
+        bool jumpInput = (Input.GetButtonDown(Constants.Input.Keys.jump) || joystick.Vertical > 0.5f) && grounded;
+        if (jumpInput)
         {
             isJumping = true;
             if (PlayerCanMove)   // TODO: Decision - pass to GameManager
@@ -46,12 +48,25 @@ public class Player : Character
 
     private void FixedUpdate()
     {
-        if (PlayerCanMove)        // TODO: Decision - pass to GameManager
+        if (PlayerCanMove)
         {
-            float move = Input.GetAxis(Constants.Input.Axis.horizontal);
+            float move = 0;
+            if (Input.GetAxis(Constants.Input.Axis.horizontal) != 0)
+            {
+                move = Input.GetAxis(Constants.Input.Axis.horizontal);
+            }
+            else if (joystick.Horizontal >= 0.2f)
+            {
+                move = 1;
+            }
+            else if (joystick.Horizontal <= -0.2f)
+            {
+                move = -1;
+            }
             float velocity = move * speed;
             rigidBody.velocity = new Vector2(velocity, rigidBody.velocity.y);
             isRunning = (velocity != 0);
+
 
             if ((move < 0 && isFacingRight) || (move > 0 && !isFacingRight))
             {
